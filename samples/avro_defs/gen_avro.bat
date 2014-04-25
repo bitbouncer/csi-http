a60ddd22a24f39bc530b22f831d59a078e5f98ba
skarlsson <svante.karlsson@csi.se>
2014-04-25 14:57:53 +0200

    Update gen_avro.bat
    fixed the generated file names

--- samples/avro_defs/gen_avro.bat
+++ samples/avro_defs/gen_avro.bat
@@ -1,1 +1,2 @@
-avrogencpp -i twitter.json -o twitter.h -n sample
+avrogencpp -i hello_world_request.json -o hello_world_request.h -n sample
+avrogencpp -i hello_world_response.json -o hello_world_response.h -n sample
