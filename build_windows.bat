0036d0acfd97d3b01f1615ed5989d394fe8f1236
skarlsson <svante.karlsson@csi.se>
2014-04-25 19:08:09 +0200

    Update build_windows.bat
    added automatic build in addition to creation of visual studio project

--- build_windows.bat
+++ build_windows.bat
@@ -15,14 +15,8 @@ mkdir win_build64
 cd win_build64
 del CMakeCache.txt
 cmake -G "Visual Studio 12 Win64" ..
+devenv ALL_BUILD.vcxproj /Build "Debug|x64"
+devenv ALL_BUILD.vcxproj /Build "Release|x64"
 cd ..
-if NOT %errorlevel% == 0 goto errorexit
-goto cleanexit
 
-:errorexit
-set returncode=%errorlevel%
-echo There was an error during CMake (error code %returncode%).
-exit /b %returncode%
-
-:cleanexit
 
