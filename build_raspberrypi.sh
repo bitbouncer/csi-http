9876495618720aadb3adb2467144877cbbedf536
skarlsson <svante.karlsson@csi.se>
2014-04-26 19:07:44 +0200

    removed boost_locale

--- build_raspberrypi.sh
+++ build_raspberrypi.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
-rm -rf raspberrypi_debug
-rm -rf raspberrypi_release
+rm -rf build
 rm -f CMakeCache.txt
 rm -f ./lib/armeabi-v7a/*
 
 export PI_TOOLS_HOME=$PWD/../tools
-mkdir raspberrypi_release
-cd raspberrypi_release
+mkdir build
+cd build
 cmake -D__LINUX__=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchains/raspberrypi.toolchain.x64.cmake .. 
 make
 cd ..
