62ad016f8593004a86c0a124066fb7b826901370
skarlsson <svante.karlsson@csi.se>
2014-04-27 00:33:34 +0200

    moved xtools

--- build_raspberrypi.sh
+++ build_raspberrypi.sh
@@ -3,7 +3,7 @@ rm -rf build
 rm -f CMakeCache.txt
 rm -f ./lib/armeabi-v7a/*
 
-export PI_TOOLS_HOME=$PWD/../tools
+export PI_TOOLS_HOME=~/xtools/tools
 mkdir build
 cd build
 cmake -D__LINUX__=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchains/raspberrypi.toolchain.x64.cmake .. 
