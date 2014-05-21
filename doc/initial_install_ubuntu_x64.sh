75c6792e0f2c6a104c35a4a3800dd8ed9349f59a
svante karlsson <svante.karlsson@csi.se>
2014-05-21 16:30:29 +0200

    Update initial_install_ubuntu_x64.sh

--- doc/initial_install_ubuntu_x64.sh
+++ doc/initial_install_ubuntu_x64.sh
@@ -1,1 +1,1 @@
-sudo apt-get install git wget cmake unzip emacs build-essential g++ python-dev autotools-dev libicu-dev zlib1g-dev openssl libssl-dev libcurl4-openssl-dev libbz2-dev libcurl4
+sudo apt-get install git wget cmake unzip emacs build-essential g++ python-dev autotools-dev libicu-dev zlib1g-dev openssl libssl-dev libcurl4-openssl-dev libbz2-dev libcurl3
