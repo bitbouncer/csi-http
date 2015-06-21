7be8fc3470f7f3eb9e3ac12103e9f7d2acaf184f
svante karlsson <svante.karlsson@csi.se>
2015-06-21 10:50:44 +0200

    release build

--- build_linux.sh
+++ build_linux.sh
@@ -4,7 +4,7 @@ rm -rf lib
 rm -rf linux
 mkdir linux
 cd linux
-cmake -D__CSI_HAS_OPENSSL__=1  -D__LINUX__=1 ..
+cmake -DCMAKE_BUILD_TYPE=Release -D__CSI_HAS_OPENSSL__=1  -D__LINUX__=1 ..
 make -j8
 cd ..
 
