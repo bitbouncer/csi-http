43b0c4e495e27aee568da1185ae40c7f9ac99511
svante karlsson <svante.karlsson@csi.se>
2015-11-02 15:33:24 +0100

    updated raspberry port

--- build_cross_raspbian.sh
+++ build_cross_raspbian.sh
@@ -5,9 +5,11 @@ rm -rf lib
 rm -rf build
 rm -f CMakeCache.txt
 rm -f ./lib/armeabi-v7a/*
+
 mkdir build
 cd build
-cmake -D__CSI_HAS_OPENSSL__=1  -D__LINUX__=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchains/raspberrypi.toolchain.x64.cmake .. 
+cmake -D__RASPBIAN__=1 -D__CSI_HAS_OPENSSL__=1 -D__LINUX__=1 -DCMAKE_TOOLCHAIN_FILE=../csi-build-scripts/toolchains/raspberry.ia32.cmake ..
 make -j4
 cd ..
+
 
